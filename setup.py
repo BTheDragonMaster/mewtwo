f3209629c55e49fc0f11aae4b167ec3e72cf0cdc
Terlouw, Barbara <barbara.terlouw@wur.nl>
2025-04-18 14:49:05 +0200

    Add setup scripts

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'MEWTWO: mRNA Expression Wizard 2'
+LONG_DESCRIPTION = "3'UTR analysis software"
+
+setup(
+    name="mewtwo",
+    version=VERSION,
+    author="Barbara Terlouw",
+    author_email="barbara.terlouw@wur.nl",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages())
