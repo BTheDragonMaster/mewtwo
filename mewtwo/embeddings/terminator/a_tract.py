5256f529b0b2670720ef7fe47b662731a9349cb7
Terlouw, Barbara <barbara.terlouw@wur.nl>
2025-04-28 15:37:11 +0200

    Bugfix: a tract vector now uses a_tract_size argument for indexing sequence

--- mewtwo/embeddings/terminator/a_tract.py
+++ mewtwo/embeddings/terminator/a_tract.py
@@ -1,6 +1,17 @@
 from mewtwo.embeddings.sequence import RNASequence
+from mewtwo.embeddings.bases import Base, base_to_vector
 
 
 class ATract:
     def __init__(self, sequence: RNASequence) -> None:
         self.sequence = sequence
+
+    def to_vector(self, a_tract_size: int = 10, one_hot: bool = False) -> list[int]:
+        vector = []
+        if len(self.sequence) < a_tract_size:
+            for i in range(a_tract_size - len(self.sequence)):
+                vector.extend(base_to_vector(Base.ZERO_PADDING, one_hot))
+        for base in self.sequence[-a_tract_size:]:
+            vector.extend(base_to_vector(base, one_hot))
+
+        return vector
---
+++ mewtwo/embeddings/terminator/test/test_a_tract.py
@@ -0,0 +1,17 @@
+import unittest
+
+from mewtwo.embeddings.terminator.a_tract import ATract
+from mewtwo.embeddings.sequence import RNASequence
+
+
+class TestATract(unittest.TestCase):
+    def test_to_vector(self):
+        a_tract_1 = ATract(RNASequence("ACGU"))
+
+        self.assertEqual(a_tract_1.to_vector(4), [1, 0, 2, 0, 1, 3, 1, 0, 3, 0, 1, 2])
+        self.assertEqual(a_tract_1.to_vector(3), [0, 1, 3, 1, 0, 3, 0, 1, 2])
+        self.assertEqual(a_tract_1.to_vector(5), [0, 0, 0, 1, 0, 2, 0, 1, 3, 1, 0, 3, 0, 1, 2])
+
+
+if __name__ == "__main__":
+    unittest.main()
