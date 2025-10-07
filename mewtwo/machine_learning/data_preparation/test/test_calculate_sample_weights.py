ad2f6c7e641b793c3156b924c95a0c4cff425325
Terlouw, Barbara <barbara.terlouw@wur.nl>
2025-10-07 10:40:41 +0200

    Code cleanup

--- mewtwo/machine_learning/data_preparation/test/test_calculate_sample_weights.py
+++ mewtwo/machine_learning/data_preparation/test/test_calculate_sample_weights.py
@@ -3,6 +3,7 @@ from math import isclose
 
 from mewtwo.machine_learning.data_preparation.calculate_sample_weights import get_sample_weights
 
+
 class CalculateSampleWeights(unittest.TestCase):
     def test_get_sample_weights(self):
         dataset_1 = [0.1, 0.3, 0.5, 0.7, 0.9]
@@ -20,7 +21,6 @@ class CalculateSampleWeights(unittest.TestCase):
             element_2 = list_2[i]
             if not isclose(element_1, element_2, abs_tol=0.00001):
                 self.fail(f"Lists are not equal: {list_1}, {list_2}. \n First mismatching element: {i} ([{element_1}], [{element_2}])")
-
 
 
 if __name__ == "__main__":
